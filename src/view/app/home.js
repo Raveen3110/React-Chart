d7e4d9d83eb04a6fdc195170ee787ec41470bc05
Aman Sharma <aman@aidetic.in>
2023-02-06 19:44:33 +0530

    added style.css

--- src/view/app/home.js
+++ src/view/app/home.js
@@ -1,20 +1,15 @@
 import React from "react";
+import '../../assets/css/style.css';
 import LineGraph from "../../components/LineGraph";
 import BarGraph from "../../components/BarGraph";
 import dataGraph from "../../common/data";
 export default function Home() {
     return (
         <>
-            <div style={{ backgroundColor: "#F6F7F4" }}>
-                <div style={{ width: "750px" }}>
-                    <LineGraph data={dataGraph.line_graph_one} />
-                </div>
-                <div style={{ width: "750px" }}>
-                    <LineGraph data={dataGraph.line_graph_two} />
-                </div>
-                <div style={{ width: "750px" }}>
-                    <BarGraph data={dataGraph.line_graph_two} />
-                </div>
+            <div className="charts">
+                <LineGraph data={dataGraph.line_graph_one} />
+                <LineGraph data={dataGraph.line_graph_two} />
+                <BarGraph data={dataGraph.line_graph_two} />
             </div>
         </>
     );
