89d8f50af3650bee3cea185c1895bcdd97812914
Raveen Deep Singh <raveendeep3101@gmail.com>
2023-02-06 17:58:36 +0530

    data add in common

---
+++ src/common/data.js
@@ -0,0 +1,61 @@
+const dataGraph = {
+    line_graph_one: {
+        labels: [
+            "Jan",
+            "Feb",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ],
+        datasets: [
+            {
+                label: 'Line Chart with Fill',
+                data: [80, 85, 30, 150, 85, 40, 10.5, 32, 50, 60, 20, 250, 140],
+                backgroundColor: "#E3F3BB",
+                borderColor: "gray",
+                tension: 0.2,
+                fill: true,
+                // pointStyle: "rect",
+                pointBackgroundColor: "red",
+                borderWidth: 2,
+            },
+        ],
+    },
+    line_graph_two: {
+        labels: [
+            "Jan",
+            "Feb",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ],
+        datasets: [
+            {
+                label: "Line Graph",
+                data: [140,10,100,80,70,60, 20, 75, 10.5, 32, 50, 140],
+                backgroundColor: "#B1DFDE",
+                borderColor: "green",
+                // tension: 0.2,
+                fill: false,
+                pointStyle: "rect",
+                pointBackgroundColor: "red",
+                borderWidth: 1,
+            },
+        ],
+    },
+};
+export default dataGraph;
--- src/view/app/home.js
+++ src/view/app/home.js
@@ -1,44 +1,17 @@
-import React, { useState } from "react";
-import BarGraph from "../../components/BarGraph";
+import React from "react";
 import LineGraph from "../../components/LineGraph";
-
+import dataGraph from "../../common/data";
 export default function Home() {
-    const [data, setData] = useState({
-        labels: [
-            "Jan",
-            "Feb",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December",
-        ],
-        datasets: [
-            {
-                label: "Line Graph with Fill",
-                data: [
-                    80, 85, 30, 150, 85, 40, 10.5, 32, 50, 60, 20, 250, 140
-                ],
-                backgroundColor: "yellow",
-                borderColor: "red",
-                tension: 0.2,
-                fill: true,
-                pointStyle: "rect",
-                pointBackgroundColor: "blue",
-            },
-        ],
-    });
     return (
-        <div>
-            <div style={{ width: "800px", height: "800px" }}>
-                <LineGraph data={data} />
+        <>
+            <div style={{ backgroundColor: "#F6F7F4" }}>
+                <div style={{ width: "750px" }}>
+                    <LineGraph data={dataGraph.line_graph_one} />
+                </div>
+                <div style={{ width: "750px" }}>
+                    <LineGraph data={dataGraph.line_graph_two} />
+                </div>
             </div>
-            <BarGraph />
-        </div>
+        </>
     );
 }
